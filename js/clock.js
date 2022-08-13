d4a8de909c9a7b0c134cf8857c52f3dd7d312118
yangdudel <66849157+ryulh@users.noreply.github.com>
2022-08-13 23:19:15 +0900

    Create base folders/files

---
+++ js/clock.js
@@ -0,0 +1,20 @@
+const clockTitle = document.querySelector("#js-clock");
+
+function getTime() {
+  const date = new Date();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  const ampm = hour < 12 ? "AM" : "PM";
+
+  clockTitle.innerText = `${hmsFormat(hour % 12)}:${hmsFormat(min)}:${hmsFormat(
+    sec
+  )} ${ampm}`;
+}
+
+function hmsFormat(time) {
+  return String(time).padStart(2, "0");
+}
+
+getTime();
+setInterval(getTime, 1000);
