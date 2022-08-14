13f75381573afcefbff88c915b86ffeeb4553a55
yangdudel <66849157+ryulh@users.noreply.github.com>
2022-08-15 02:57:34 +0900

    fix bugs1

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 const imgNums = 7;
 const background = document.getElementById("background");
-const imgStr = `background/${Math.floor(Math.random() * imgNums + 1)}.jpg`;
+const imgStr = `./background/${Math.floor(Math.random() * imgNums + 1)}.jpg`;
 
 background.style.backgroundImage = `url(${imgStr})`;
